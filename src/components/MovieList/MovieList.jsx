b06a671202b15f083b6eac322dde6d1d57255c44
Jerome-Horton <riqui.howard@gmail.com>
2021-12-12 02:19:46 -0600

    working throught Details Page error

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
+import MovieItem from '../MovieItem/MovieItem';
 
 function MovieList() {
 
@@ -8,19 +9,19 @@ function MovieList() {
     const movies = useSelector(store => store.movies);
 
     useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
+        dispatch({ 
+            type: 'FETCH_MOVIES' 
+        });
+
     }, []);
 
     return (
         <main>
-            <h1>MovieList</h1>
+            <h1>Movie List</h1>
             <section className="movies">
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
-                        </div>
+                            <MovieItem movie={movie}/>
                     );
                 })}
             </section>
